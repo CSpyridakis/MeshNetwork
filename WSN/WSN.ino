1f308fdc7d36115fc0d26aa9bd5c7713a9e2d992
gstamatakis <giorgoshstam@gmail.com>
2017-09-22 11:35:24 +0300

    Added documentation

--- WSN/WSN.ino
+++ WSN/WSN.ino
@@ -1,29 +1,26 @@
 #include <easyMesh.h>
 
-// some gpio pin that is connected to an LED... 
-// on my rig, this is 5, change to the right number of your LED.
 #define   LED             5       // GPIO number of connected LED
 
 #define   BLINK_PERIOD    1000000 // microseconds until cycle repeat
 #define   BLINK_DURATION  100000  // microseconds LED is on for
 
-#define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneeky"
+#define   MESH_PREFIX     "mesh"
+#define   MESH_PASSWORD   "12345678"
 #define   MESH_PORT       5555
 
-easyMesh  mesh;
+easyMesh  mesh;   //The mesh handler
 
 uint32_t sendMessageTime = 0;
 
 void setup() {
 	Serial.begin(115200);
-
 	pinMode(LED, OUTPUT);
-
-	//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-	mesh.setDebugMsgTypes(ERROR | STARTUP);  // set before init() so that you can see startup messages
+ 
+	mesh.setDebugMsgTypes(ERROR | STARTUP);  // Allowed types are: ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE
 
 	mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+ 
 	mesh.setReceiveCallback(&receivedCallback);
 	mesh.setNewConnectionCallback(&newConnectionCallback);
 
@@ -59,9 +56,10 @@ void loop() {
 }
 
 void receivedCallback(uint32_t from, String &msg) {
-	Serial.printf("startHere: Received from %d msg=%s\n", from, msg.c_str());
+	Serial.printf("Received from %d msg=%s\n", from, msg.c_str());
 }
 
 void newConnectionCallback(bool adopt) {
-	Serial.printf("startHere: New Connection, adopt=%d\n", adopt);
+	Serial.printf("New Connection, adopt=%d\n", adopt);
+  Serial.printf("Connection count: %d\n", mesh.connectionCount() );
 }
