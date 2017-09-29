2fcd92214a2c45983e5dd842e6fb03720213335c
gstamatakis <giorgoshstam@gmail.com>
2017-09-29 20:36:35 +0300

    Refactoring.Added an extra timer.Events are produced and consumed on different timers now.

--- WSN/WSN.ino
+++ WSN/WSN.ino
@@ -4,12 +4,13 @@
 #define SENSOR_NO 3
 #define ANALOGPIN A0
 #define DHTPin    12 //GPIO 12
-#define DHTTYPE DHT11
+#define DHTTYPE   DHT11
+#define LED       5             // GPIO number of connected LED.
 
 //Mesh vars
-#define   LED                     5             // GPIO number of connected LED.
-#define   MESH_UPDATE_INTERVAL    1000L         // microseconds until cycle repeat
-#define   BROADCAST_INTERVAL      5000L         // microseconds between each broadcast
+#define   MESH_UPDATE_INTERVAL    500L          // microseconds between each broadcast
+#define   SENSOR_UPDATE_INTERVAL  1000L         // microseconds between each sensor update
+#define   BROADCAST_INTERVAL      1000L         // microseconds between each broadcast
 
 #define   MESH_PREFIX     "mesh"
 #define   MESH_PASSWORD   "12345678"
@@ -18,13 +19,34 @@
 static easyMesh  mesh;   
 
 os_timer_t meshUpdateTimer;   //Maintenance mesh network tasks
-os_timer_t readingsTimer;     //Readings from sensor
+os_timer_t readingsTimer;     //Readings from sensor (locally)
+os_timer_t broadcastTimer;    //Broadcast local sensor values.
 
+//*********************** DHT 11 ****************************
+// Reading temperature or humidity takes about 250 milliseconds!
+// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 DHT dht(DHTPin, DHTTYPE);
+float DHT_temperature,DHT_heatIndex,DHT_humidity;
+
+//*********************** MQ-135 ****************************
+//A gas sensor.Needs about an hour of use to produce valid readings
+int gasVal;
+
+
+//*********************** LDR photoresistor *****************
+int LDRval;
+
+
 
 void setup() {
-	Serial.begin(115200);
-	pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
  
   switch(SENSOR_NO){  //Initialize the correct sensors. 
     case 1:
@@ -34,106 +56,105 @@ void setup() {
       break;
   }
   
-  // Allowed types are: ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE 
-	mesh.setDebugMsgTypes(ERROR | STARTUP);  
-	mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+  // ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE 
+  mesh.setDebugMsgTypes(ERROR | STARTUP);  
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
 
-  //Attach the needed callbacks
-	mesh.setReceiveCallback(&receivedCallback);
-	mesh.setNewConnectionCallback(&newConnectionCallback);
+  //Attach the mesh callbacks
+  mesh.setReceiveCallback(&receivedCallback);
+  mesh.setNewConnectionCallback(&newConnectionCallback);
 
   timers_init();
 }
 
 void loop() {
-  yield();
+  ESP.wdtFeed();
 }
 
-//Timer tasks and init_timer funcs.
+//Timer task that updates the mesh
 void meshUpdate_timer_task() {
     mesh.update();
 }
 
+//Timer tasks that gets and stores locally the sensor readings.
 void getReadings_timer_task() {
+    noInterrupts();
     getReadings();
+    interrupts();
 } 
-   
+
+//Timer tasks that broadcasts the sensor readings.
+void broadcastReadings_timer_task() {
+    broadcastReadings();
+} 
+
+//Disarms,attaches callbacks and finally arms the timers.
+//Call this method in setup().
 void timers_init(void) {
     os_timer_disarm(&meshUpdateTimer);
     os_timer_disarm(&readingsTimer);
-    
+    os_timer_disarm(&broadcastTimer);
+
     os_timer_setfn(&meshUpdateTimer,(os_timer_func_t *) meshUpdate_timer_task, NULL); //Schedule the tasks
     os_timer_setfn(&readingsTimer,(os_timer_func_t *) getReadings_timer_task, NULL);
+    os_timer_setfn(&broadcastTimer,(os_timer_func_t *) broadcastReadings_timer_task, NULL);
 
-    os_timer_arm(&meshUpdateTimer, MESH_UPDATE_INTERVAL, true); //3rd arg -> Repeat task
-    os_timer_arm(&readingsTimer, BROADCAST_INTERVAL, true);
+    //If the 3rd arg is true then the task is executed periodically.
+    os_timer_arm(&meshUpdateTimer, MESH_UPDATE_INTERVAL, true); 
+    os_timer_arm(&readingsTimer, SENSOR_UPDATE_INTERVAL, true);
+    os_timer_arm(&broadcastTimer, BROADCAST_INTERVAL, true);
 }
 
 void receivedCallback(uint32_t from, String &msg) {
-	Serial.printf("Received from %d : %s\n", from, msg.c_str());
+  Serial.printf("Received from %d : %s\n", from, msg.c_str());
 }
 
 void newConnectionCallback(bool adopt) {
-	Serial.printf("New Connection, adopt=%d\n", adopt);
+  Serial.printf("New Connection, adopt=%d\n", adopt);
   Serial.printf("Connection count: %d\n", mesh.connectionCount() );
 }
 
-//Select this method only when DHT11 is attached!
-String getDHTreadings(){
-            char celsiusTemp[7];
-            char fahrenheitTemp[7];
-            char humidityTemp[7];
-            float hic;
-            float hif; 
-             
-            // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-            // Read the humidity value.
-            float h = dht.readHumidity();
-            // Read temperature as Celsius (the default)
-            float t = dht.readTemperature();
-            // Read temperature as Fahrenheit (isFahrenheit = true)
-            float f = dht.readTemperature(true);
-            // Check if any reads failed and exit early (to try again).
-            if (isnan(h) || isnan(t) || isnan(f)) {    
-              return "DHT failed.";   
-            }else{
-              // Computes temperature values in Celsius + Fahrenheit and Humidity
-              hic = dht.computeHeatIndex(t, h, false);       
-              dtostrf(hic, 6, 2, celsiusTemp);             
-              hif = dht.computeHeatIndex(f, h);
-              dtostrf(hif, 6, 2, fahrenheitTemp);         
-              dtostrf(h, 6, 2, humidityTemp);
-            }   
-            return "  C:"+String(celsiusTemp)+" F:"+String(fahrenheitTemp)+"  H:"+String(humidityTemp)+"%";
+//Broadcasts the existing values of the sensor(s) produced by getReadings().
+//Uses interrupts. DO NOT call inside an ISR, instead use getReadings to
+//obtain the values and then publish the results with this method.
+void broadcastReadings(){
+  String msg;
+  switch(SENSOR_NO){
+    case 1://DHT
+      msg = "C:"+String(DHT_temperature)+"  H:"+String(DHT_humidity) + "  HIC:"+String(DHT_heatIndex);
+      break;
+    case 2://LDR
+      msg = "LDR: " + String(LDRval); 
+      break;
+    case 3://MQ-135
+      msg = "Gas PPM: " + String(gasVal);
+      break;
+    default:
+      msg ="Error at publishValues default statement.";
+      break;
+  }
+  mesh.sendBroadcast(msg);
+  Serial.println(msg);
+  Serial.flush();
 }
 
-String getPhotoresistorReadings(){
-  int sensorValue = analogRead(ANALOGPIN);
-  float value = sensorValue * (100 / 1023.0);
-  return "Light: " + String(value) + "%";  
-}
-
-String getGasReadings(){
-  return "Gas PPM:" + String(analogRead(ANALOGPIN));
-}
-
-//This method sends data from the correct sensor(s).
+//This method SAVES locally the sensor results.
+//Very-fast routine without interrupts, can be called by an ISR.
 void getReadings(){
-    String message = "";
     switch(SENSOR_NO){
       case 1: //DHT11
-        message = getDHTreadings();
+         DHT_humidity = dht.readHumidity();
+         DHT_temperature = dht.readTemperature();
+         DHT_heatIndex = dht.computeHeatIndex(DHT_temperature, DHT_humidity, false); //Heat Index
         break;
       case 2: //Photoresistor
-        message = getPhotoresistorReadings();
+        LDRval = analogRead(ANALOGPIN);
         break;      
       case 3: //MQ135
-        message = getGasReadings();
+        gasVal = analogRead(ANALOGPIN);
         break;      
       default:
-        message = "Failed to get a reading";
+        break;
     }
-    mesh.sendBroadcast(message);  //Send to mesh
-    Serial.println(message);      //Send to self
 }
 
